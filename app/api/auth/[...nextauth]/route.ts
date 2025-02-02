31362e648897a195e2b216a9f2246ab9c17b89e2
HuyVo1231 <vonhathuy1306@gmail.com>
2025-02-02 09:25:53 +0700

    fix type nextauth 13+

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import bcrypt from 'bcrypt'
-import NextAuth, { AuthOptions } from 'next-auth'
+import NextAuth, { NextAuthOptions } from 'next-auth'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
@@ -7,7 +7,7 @@ import CredentialsProvider from 'next-auth/providers/credentials'
 
 import prisma from '@/app/libs/prismadb'
 
-export const authOptions: AuthOptions = {
+export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
